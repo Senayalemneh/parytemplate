7a2a2ed3447a44795324850fd0cc5437316347d0
YohannesAberaSeyoum <yohannesabera1236@gmail.com>
2026-04-13 08:19:26 +0300

    api url

--- src/utils/getApiUrl.ts
+++ src/utils/getApiUrl.ts
@@ -1,8 +1,9 @@
 export const getApiUrl = () => {
   // const API_URL ="https://bole.prosperity.codenilesolutions.com/api/";
   //  const API_URL ="https://bole.prosperity.boleprosperityparty.org/api/";
-   const API_URL ="https://prosperity-dev.boleprosperityparty.org/api/";
- 
-    
+  //  const API_URL ="https://prosperity-dev.boleprosperityparty.org/api/";
+  const API_URL = "http://localhost:3000/api/";
+
+
   return API_URL;
 };
